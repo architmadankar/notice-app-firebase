19c51602ceaa2ef5a71b95223bae9154460f1c93
Archit Madankar <51399690+architmadankar@users.noreply.github.com>
2022-11-06 20:25:35 +0530

    configure express server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hi");
+const express = require('express')
+const app = express()
+
+const port = 4000;
+
+function handleListening(){
+    console.log('started on http://localhost:'+port);
+}
+
+app.listen(port, handleListening);
