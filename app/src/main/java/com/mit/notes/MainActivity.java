55900aa42ca5850c6ebcf0f971ce006a7e4dc7ca
Archit Madankar <51399690+architmadankar@users.noreply.github.com>
2022-11-19 18:01:16 +0530

    Frontend Integration of MainActivity

--- app/src/main/java/com/mit/notes/MainActivity.java
+++ app/src/main/java/com/mit/notes/MainActivity.java
@@ -2,13 +2,63 @@ package com.mit.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText mloginemail,mloginpassword;
+    private RelativeLayout mlogin,mgotosignup;
+    private TextView mgotoforgotpassword;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+        mloginemail=findViewById(R.id.Loginemail);
+        mloginpassword=findViewById(R.id.Loginpassword);
+        mlogin=findViewById(R.id.Login);
+        mgotoforgotpassword=findViewById(R.id.gotoforgotpassword);
+        mgotosignup=findViewById(R.id.gotosignup);
+
+
+        mgotosignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent( MainActivity.this,signup.class));
+            }
+        });
+
+        mgotoforgotpassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent( MainActivity.this,forgotpassword.class));
+            }
+        });
+
+        mlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            String mail=mloginemail.getText().toString().trim();
+            String password=mloginpassword.getText().toString().trim();
+
+            if (mail.isEmpty() || password.isEmpty()){
+                Toast.makeText(getApplicationContext(),"Mail and password needs to be Entered",Toast.LENGTH_SHORT).show();
+
+
+            }
+            else{
+                //login the user
+            }
+            }
+        });
     }
 }
