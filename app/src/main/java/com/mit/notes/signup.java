101a7eda842a5864ce761bff086aeea231f22427
Archit Madankar <51399690+architmadankar@users.noreply.github.com>
2022-11-20 01:36:01 +0530

    Signup class init

--- app/src/main/java/com/mit/notes/signup.java
+++ app/src/main/java/com/mit/notes/signup.java
@@ -2,13 +2,56 @@ package com.mit.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class signup extends AppCompatActivity {
+
+    private EditText msignupemail,msignuppassword;
+    private Button msignup,mgotologin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        getSupportActionBar().hide();
+
+        msignupemail=findViewById(R.id.signupemail);
+        msignuppassword=findViewById(R.id.signuppassword);
+        msignup=findViewById(R.id.signup);
+        mgotologin=findViewById(R.id.gotologin);
+
+        mgotologin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(signup.this,MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+        msignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mail=msignupemail.getText().toString().trim();
+                String password=msignuppassword.getText().toString().trim();
+
+                if (mail.isEmpty() || password.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Fill the required Fields",Toast.LENGTH_SHORT).show();
+                }else if(password.length()<7){
+                    Toast.makeText(getApplicationContext(),"Password should be greater than 7 digits",Toast.LENGTH_SHORT).show();
+
+                }
+                else{
+                    //Register the user to firebase
+                }
+            }
+        });
     }
 }
