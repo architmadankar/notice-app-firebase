5851e3f791ed9817f5ac881310d9c1048f0a64c6
Dnyanada <khandaitdnyanada@gmail.com>
2022-11-20 19:09:51 +0530

    creating Activities and Classes

--- app/src/main/java/com/mit/notes/createnote.java
+++ app/src/main/java/com/mit/notes/createnote.java
@@ -3,12 +3,37 @@ package com.mit.notes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class createnote extends AppCompatActivity {
+    EditText mcreatetitleofnote,mcreatecontentofnote;
+    FloatingActionButton msavenote;
+
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+    FirebaseFirestore firebaseFirestore;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_createnote);
+
+        msavenote=findViewById(R.id.savenote);
+        mcreatecontentofnote=findViewById(R.id.createcontentonote);
+        mcreatetitleofnote=findViewById(R.id.createtitleofnote);
+
+        Toolbar toolbar=findViewById(R.id.toolbarofcreateote);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        
+
     }
 }
