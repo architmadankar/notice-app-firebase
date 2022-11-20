8c698dfcb9b89b2291b7607d48d817b8896fe102
Dnyanada <khandaitdnyanada@gmail.com>
2022-11-20 19:30:00 +0530

    Firestore Integration // createnote.class

--- app/src/main/java/com/mit/notes/createnote.java
+++ app/src/main/java/com/mit/notes/createnote.java
@@ -1,15 +1,28 @@
 package com.mit.notes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class createnote extends AppCompatActivity {
     EditText mcreatetitleofnote,mcreatecontentofnote;
@@ -33,7 +46,62 @@ public class createnote extends AppCompatActivity {
         Toolbar toolbar=findViewById(R.id.toolbarofcreateote);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        
+
+
+        firebaseAuth=FirebaseAuth.getInstance();
+        firebaseFirestore=FirebaseFirestore.getInstance();
+        firebaseUser=FirebaseAuth.getInstance().getCurrentUser();
+
+        msavenote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String title=mcreatetitleofnote.getText().toString();
+                String content=mcreatecontentofnote.getText().toString();
+                if (title.isEmpty() || content.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Fill Both fields",Toast.LENGTH_SHORT).show();
+
+                }else{
+
+                    DocumentReference documentReference=firebaseFirestore.collection("notes").document(firebaseUser.getUid()).collection("myNotes")
+                    Map<String, Object> note=new HashMap<>();
+                    note.put("title",title);
+                    note.put("content,"content);
+
+                    documentReference.set(note).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+
+                            Toast.makeText(getApplicationContext(),"Note Created Successfully",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(createnote.this,notesactivity.class));
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getApplicationContext(),"Failed to create Note",Toast.LENGTH_SHORT).show();
+                            //startActivity(new Intent(createnote.this,notesactivity.class));
+
+                        }
+                    });
+
+                }
+            }
+        });
+
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId()==android.R.id.home){
+            onBackPressed();
+        }
+
+        return super.onOptionsItemSelected(item);
+
+
 
     }
 }
